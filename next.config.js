297f127a489544c397b6ec013e1f86b6bbd72d7b
Travis Miller <travis@travismiller.com>
2021-02-01 16:20:41 -0600

    Add workflow actions for GitHub pages

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+
+module.exports = {
+  // https://nextjs.org/docs/api-reference/next.config.js/basepath
+  basePath: process.env.BASE_PATH || '',
+}
