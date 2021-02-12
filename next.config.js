f7810d76c2ea2199664da889a6810bf8db11e37e
Travis Miller <travis@travismiller.com>
2021-02-12 13:56:00 -0600

    Add Cloudinary image loader

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const withImages = require('next-images')
 module.exports = withImages({
   // https://nextjs.org/docs/api-reference/next.config.js/basepath
   basePath: process.env.BASE_PATH || '',
+
+  // https://nextjs.org/docs/basic-features/image-optimization
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/rnatulsa/image/upload/'
+  },
 })
