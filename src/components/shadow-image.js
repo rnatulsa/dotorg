0b8c25995e6c8b0d2c645ea8a5cb57e306765e78
Travis Miller <travis@travismiller.com>
2022-04-09 13:44:37 -0500

    Add 2022 Garage Sale

--- src/components/shadow-image.js
+++ src/components/shadow-image.js
@@ -1,10 +1,10 @@
 import classNames from 'classnames'
 import styles from '@/styles/shadow-image.module.css'
 
-export default function ShadowImage({ src, className, ...props }) {
+export default function ShadowImage({ src, className, alt, title, ...props }) {
   return (
     <div className={classNames([styles.shadowImage, className])} {...props}>
-      <img src={src} />
+      <img src={src} alt={alt} title={title} />
     </div>
   )
 }
