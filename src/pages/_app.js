ac91018c8ce1c30c81af20d8ed51994b3cb378b1
Travis Miller <travismiller@users.noreply.github.com>
2023-05-04 21:38:25 -0500

    Add Google Analytics (#28)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { GlobalStyles } from 'twin.macro'
 import Title from '@/components/title'
 import Head from 'next/head'
 import Layout from '@/components/layout'
+import Analytics from '@/components/analytics'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -15,6 +16,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&amp;display=swap" rel="stylesheet" />
       </Head>
+
+      <Analytics />
 
       <GlobalStyles />
 
