c1a90832a33e38af083c70fc75ac72b5d91a7676
Travis Miller <travis@travismiller.com>
2024-04-16 16:30:01 -0500

    update sale page for 2024

--- src/pages/sale.js
+++ src/pages/sale.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '@/components/title'
 import Link from 'next/link'
-import MidtownYardSale from '@/images/events/yard-sale/2023/midtown-yard-sale-2023.jpg'
+import MidtownYardSale from '@/images/events/yard-sale/2024/midtown-yard-sale-2024.png'
 
 export default function Sale() {
   return <>
@@ -9,13 +9,15 @@ export default function Sale() {
 
     <div className="; container mx-auto p-8 text-center flex-col space-y-4">
       <h1 className="; text-5xl font-bold">Midtown Yard&nbsp;Sale Extravaganza</h1>
-      <p>The Yard Sale has ended. Thanks for everyone who participated and stopped by!</p>
-      <p><Link href="/events">Upcoming Events</Link></p>
+      <h2 className="; text-3xl font-bold">June 7-9, 2024</h2>
+      <p>The Renaissance Neighborhood Association has partnered with Florence Park to hold a multi-neighborhood-wide yard sale.</p>
+      <p>There is no cost for you to participate! Just gather your treasures and display them in your yard or garage. The Neighborhood Associations will advertise in the Tulsa World, Craigslist, FB Marketplace/Groups and other online sites. The associations will also put out several big signs around key locations of our neighborhood but you will need to put out signs to direct traffic to your specific location. You can choose to participate on any or all of the days.</p>
+      <p>Sign up to participate here: <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSf6fGzZMzRti7lttQI4wCx2xD3psqh8YUQ-NTaEv5oqHRuDSQ/viewform">Google Form</a></p>
+      <p>Once you sign up, your address will be placed on the sale map that will be accessible to potential buyers.</p>
       <p>
-        <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/WFvvUu6PgNtw1M4D9">Sign up</a> to receive information about next year’s sale.
-      </p>
-      <p>
-        <img className="; mx-auto" src={MidtownYardSale} alt="Midtown Yard Sale - Hosted by Renaissance &amp; Florence Park" />
+        <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSf6fGzZMzRti7lttQI4wCx2xD3psqh8YUQ-NTaEv5oqHRuDSQ/viewform">
+          <img className="; mx-auto" src={MidtownYardSale} alt="Midtown Yard Sale - Hosted by Renaissance &amp; Florence Park" />
+        </a>
       </p>
     </div>
   </>
